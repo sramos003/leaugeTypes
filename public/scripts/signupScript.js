b19cbc1f8b2af56103ab8eddd217aeb9a7a98f93
Steve Ramos <rsteve910@gmail.com>
2019-09-03 17:27:50 -0400

    End of day progress

--- public/scripts/signupScript.js
+++ public/scripts/signupScript.js
@@ -2,22 +2,20 @@ $(function() {
     let url = new URLSearchParams(location.search);
     let teamId = url.get("id");
     let teamData;
-    loadCard(teamId);
+    loadTeamCard(teamId);
     boxEffects(teamId);
     submitForm(teamId);
+    boxValidation();
+    $('#checkBox').hide();
 });
 
-function loadCard(id) {
+function loadTeamCard(id) {
     $.getJSON(`/api/teams/${id}`, (data) => {
         let cardTopHeader = $('#teamLeague');
         let cardBodyHeader = $('#bodyTop');
         let cardContainer = $('#bodyContainer');
         cardTopHeader.html(`${data.League}`);
         cardBodyHeader.html(`-<u>${data.TeamName}</u>-`);
-        cardContainer.append($('<p />')
-            .attr('value', 'card-text largePoint')
-            .html(`Manager Name: ${data.ManagerName}`)
-        );
         cardContainer.append($('<p />')
             .attr('class', ' card-text largePoint')
             .html(`Team Gender: ${data.TeamGender}`)
@@ -26,6 +24,7 @@ function loadCard(id) {
             .attr('class', ' card-text memNum largePoint')
             .html(`Number of members: (${data.Members.length})`)
         );
+        loadMemberCard(data);
     });
 };
 
@@ -40,17 +39,6 @@ function boxEffects(id) {
             $(this).addClass('shadow-lg');
         }
     });
-    //All Texboes on focus
-    $('input[type="text"]').on({
-        focus: function() {
-            $(this).addClass('focusColor');
-            $('.toast').hide();
-        },
-        blur: function() {
-            $(this).removeClass('focusColor');
-        }
-    });
-    //When Phone textbox focused, changes to phoneNum format
     $('input[name="phone"]').on({
         focus: function() {
             $(this).attr('placeholder', '(810)-123-4321')
@@ -59,131 +47,185 @@ function boxEffects(id) {
             $(this).attr('placeholder', 'Your Number');
         }
     });
-    $('.num').on({
+};
+
+function boxValidation() {
+    //All Texboes on fo
+    $('input[type="text"]').on({
         focus: function() {
+            $(this).addClass('focusColor');
             $(this).removeClass('errorRed');
             $('#errorRmv').remove();
         },
         blur: function() {
+            $('input[type="text"]').removeClass('focusColor');
+        }
+    });
+    //Textbox on blur validation
+    $('#memberName').on({
+        focus: function() {
+            $('#errorRmv').remove();
+        },
+        blur: function() {
+            if (!$(this).val().trim()) {
+                $(this).addClass('errorRed')
+                $(this).after(`<div id="errorRmv"><small class="text-danger">&nbsp; Please enter a name</small></div>`)
+                $(this).html('');
+            };
+        }
+    });
+    $('#age').on({
+        focus: function() {
+            $('#errorRmv').remove();
+        },
+        blur: function() {
             let numReg = /^[0-9]*$/;
-            let phoneReg = /^[2-9]\d{2}(-)\d{3}(-)\d{4}$/;
-            if (!numReg.test($(this).val()) && !phoneReg.test($(this).val())) {
+
+            if (!numReg.test($(this).val()) || $(this).val() < 10 || $(this).val() >= 99) {
+                $(this).addClass('errorRed')
+                $(this).after(`<div id="errorRmv"><small class="text-danger">&nbsp;<i> Please enter a valid age between 10 & 99</i></small></div>`)
                 $(this).val('');
-                $(this).addClass('errorRed');
-                $(this).after(`<div id="errorRmv"><small class="text-danger shadow-lg">&nbsp; Please enter a valid number</small></div>`);
+            }
+        }
+    });
+    $('#memberPhone').on({
+        focus: function() {
+            $('#errorRmv').remove();
+        },
+        blur: function() {
+            let phoneReg = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/
+            if (!$(this).val().trim() || !phoneReg.test($(this).val())) {
+                $(this).addClass('errorRed')
+                $(this).after(`<div id="errorRmv"><small class="text-danger">&nbsp; Please enter a phone number</small></div>`)
+                $(this).val('');
             };
         }
     });
-    $('#memberName"]').on({
+    $('#memberEmail').on({
         focus: function() {
-            removeError($(this));
+            $('#errorRmv').remove();
+        },
+        blur: function() {
+            let emailReg = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+            if (!$(this).val().trim() || !emailReg.test($(this).val())) {
+                $(this).addClass('errorRed')
+                $(this).after(`<div id="errorRmv"><small class="text-danger">&nbsp; Please enter a valid email</small></div>`)
+                $(this).html('');
+            };
+        }
+    });
+    $('#nickName').on({
+        focus: function() {
+            $('#errorRmv').remove();
         },
         blur: function() {
             if (!$(this).val().trim()) {
-                addError($(this));
+                $(this).addClass('errorRed')
+                $(this).after(`<div id="errorRmv"><small class="text-danger">&nbsp; Please enter a name</small></div>`)
+                $(this).html('');
             };
         }
     });
-    $('#memberPhone"]').on({
-        focus: function() {
-            removeError($(this));
-        },
-        blur: function() {
-            if (!$(this).val().trim()) {
-                addError($(this));
-            };
-        }
-    });
-    $('#memberEmail"]').on({
-        focus: function() {
-            removeError($(this));
-        },
-        blur: function() {
-            if (!$(this).val().trim()) {
-                addError($(this));
-            };
-        }
-    });
-    $('#nickName"]').on({
-        focus: function() {
-            removeError($(this));
-        },
-        blur: function() {
-            if (!$(this).val().trim()) {
-                addError($(this));
-            };
-        }
-    });
-};
-
-function removeError(field) {
-    $(field).removeclass('errorRed');
-    $('#errorrmv').remove();
-};
-
-function addError(field) {
-    $(field).addClass('errorRed');
-    $(field).after(`<div id="errorRmv"><small class="text-danger shadow-lg">&nbsp; Please enter a name</small></div>`);
 };
 
 function submitForm(id) {
-    //alert("I exist ")
-    $('#submit').on('click', function(e) {
-        e.preventDefault();
-        let nameFieldA = $('#memberName').val();
-        let nameFieldB = $('#nickName').html();
-        let numberField = $('#memberPhone').val();
-        let emailField = $('#memberEmail').val();
-        let ageField = $('#age').val();
-        let validForm = validateForm(nameFieldA, nameFieldB, numberField, emailField, ageField);
-        if (validForm) {
-            $.post(`/api/teams/${id}/members`, $("#signupForm").serialize(), function() {
-                window.location.href = "teamList.html"
-            });
+    $('#submit').on({
+        click: function(e) {
+            e.preventDefault();
+            if (!$('input[name="gender"]:checked').val()) {
+                $('#endRow').after(`<div id="errorRmv"><small class="text-danger">&nbsp; Please select a gender.</small></div>`)
+            } else {
+                $('#errorRmv').remove();
+            };
+            if (!$('input[type=text]').hasClass('errorRed') && $('input[type=text]').val() != "" && $('input[name="gender"]:checked').val() != undefined) {
+                $.post(`/api/teams/${id}/members`, $("#signupForm").serialize(), function() {
+                    window.location.href = "teamList.html"
+                });
+            };
+        },
+        mouseenter: function() {
+            $('#checkBox').show();
+        },
+        mouseleave: function() {
+            $('#checkBox').hide();
         }
     });
 };
 
-function validateForm(memberName, memberContact, phone, email, age) {
-    // console.log("Im validating");
-    let errorMsg = [];
-    let errorStatus = false;
-    let temp = "";
-    // trim() - takes off trailing and leading spaces
-    if (!memberName.trim()) {
-        errorMsg[errorMsg.length] = "Please enter a Name.";
-        errorStatus = true;
+function loadMemberCard(data) {
+    defaultMemberCard(data)
+    $.each(data.Members, function(i) {
+        let member = data.Members[i];
+        $('#memberData').append($('<tr />')
+            .attr('id', `mem${member.MemberId}`)
+        );
+        $(`#mem${member.MemberId}`).append($('<td />')
+            .attr('class', 'largePoint')
+            .html(`<i class="fas fa-child"></i> &nbsp;${member.MemberName} (${member.Age}) - Years Old &nbsp; `)
+        );
+        $(`#mem${member.MemberId}`).on('click', function() {
+            $('#memberCard').empty();
+            $('#memberCard').append($('<p />')
+                .attr('class', 'card-text largePoint')
+                .html(`Member Name: ${member.MemberName}`)
+            );
+            $('#memberCard').append($('<p />')
+                .attr('class', 'card-text largePoint')
+                .html(`Nick Name ${member.ContactName}`)
+            );
+            $('#memberCard').append($('<p />')
+                .attr('class', 'card-text largePoint')
+                .html(`Email ${member.Email}`)
+            );
+        });
+    });
+};
+
+function defaultMemberCard(data) {
+
+    $('#memberCard').empty();
+    // console.log(data);
+    $('#memberCard').append($('<div />')
+        .attr('id', 'leagueRow')
+    );
+    $('#leagueRow').append($('<div />')
+        .attr('class', 'col-md-12 text-center')
+        .attr('id', 'leagueCol')
+    );
+    $('#leagueCol').append($('<div />')
+        .attr('class', 'ml-5 imgHolderC')
+        .attr('id', 'leagueImg')
+    );
+    let leagueImg = imageSwitch(data.League);
+    console.log(leagueImg);
+    $('#leagueImg').addClass(leagueImg);
+    $('#memberCard').append($('<p />')
+        .attr('class', 'card-text offset-4 largePoint')
+        .html(`Manager Info`)
+    );
+    $('#memberCard').append($('<p />')
+        .attr('class', 'card-text largePoint')
+        .html(`Name ${data.ManagerName}`)
+    );
+    $('#memberCard').append($('<p />')
+        .attr('class', 'card-text largePoint')
+        .html(`Email ${data.ManagerEmail}`)
+    );
+    $('#memberCard').append($('<p />')
+        .attr('class', 'card-text largePoint')
+        .html(`Phone Number${data.ManagerPhone}`)
+    );
+
+};
+
+function imageSwitch(league) {
+    switch (league) {
+        case "LIQ":
+            league = "stadium";
+            break;
+        case "NRG":
+            league = "second";
+            break;
     };
-    /*     if (!memeberContact.trim()) {
-            errorMsg[errorMsg.length] = "Please enter a Nick Name.";
-            errorStatus = true;
-        }; */
-    if (!email.trim()) {
-        errorMsg[errorMsg.length] = "Please enter an Email.";
-        errorStatus = true;
-    };
-    let emailReg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    if (!emailReg.test(email)) {
-        errorMsg[errorMsg.length] = "Please enter a valid Email.";
-        errorStatus = true;
-    };
-    if (!age.trim()) {
-        errorMsg[errorMsg.length] = "Please enter an Age.";
-        errorStatus = true;
-    };
-    let phoneReg = /^[2-9]\d{2}(-)\d{3}(-)\d{4}$/;
-    if (!phoneReg.test(phone)) {
-        errorMsg[errorMsg.length] = "Please enter a valid Phone Number";
-        errorStatus = true;
-    };
-    if (errorStatus == true) {
-        $('.card-body').empty();
-        for (let i = 0; i < errorMsg.length; i++) {
-            console.log("error found");
-            temp += errorMsg[i] + "</br>";
-        };
-        return false;
-    } else {
-        return true;
-    };
+    return league;
 };
